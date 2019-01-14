1b8bfc7f3835d894bee4fec1d459936b9e006fbc
matthewh806 <matthewh806@gmail.com>
2019-01-14 12:02:26 +0100

    colour output image :)

--- mandelbrot/fractal_plotter.cpp
+++ mandelbrot/fractal_plotter.cpp
@@ -9,6 +9,22 @@
 #include "fractal_plotter.hpp"
 #include <FreeImage.h>
 
+const int N = 256;
+const int N3 = N * N * N;
+
+std::tuple<int, int, int> get_rgb_piecewise_linear(int n, int iter_max) {
+    double t = (double)n/(double)iter_max;
+    
+    n = (int) ( t * (double)N3 );
+    
+    int b = n / (N*N);
+    int nn = n - b * N * N;
+    int r = nn / N;
+    int g = nn - r * N;
+    
+    return std::tuple<int, int, int>(r, g, b);
+}
+
 void plot(window<int> &scr, std::vector<int> &colors, int iter_max) {
     FreeImage_Initialise();
     
@@ -19,12 +35,12 @@ void plot(window<int> &scr, std::vector<int> &colors, int iter_max) {
         for(int j = scr.x_min(); j < scr.x_max(); j++) {
             int n = colors[k];
             
-            bool black = n == iter_max;
+            auto rgb = get_rgb_piecewise_linear(n, iter_max);
             
             RGBQUAD col;
-            col.rgbRed = black ? 0:255;
-            col.rgbBlue = black ? 0:255;
-            col.rgbGreen = black ? 0:255;
+            col.rgbRed = std::get<0>(rgb);
+            col.rgbBlue = std::get<1>(rgb);
+            col.rgbGreen = std::get<2>(rgb);
             
             FreeImage_SetPixelColor(bitmap, j, i, &col);
             k++;
